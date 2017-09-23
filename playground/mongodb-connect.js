5af4735ec3543fa7a2048c0509f9823abbfed399
yanhaozhai <zhaiyanhao2008@gmail.com>
2017-09-23 15:32:06 -0400

    test

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+const mongoClient = require('mongodb').MongoClient;
+
+var user = {name:'test',age:23};
+
+var {name}= user;
+
+mongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+    if(err){
+        return console.log("cannot connect to mongo db");
+    }
+
+    console.log("CONNECT TO MONGO DATABASE");
+
+    db.collection('Todos').insertOne({
+        text:'Something to do',
+        completed:false
+    },(err,result)=>{
+        if(err){
+            return console.log('Unable to insert todo' , err);
+        }
+
+        console.log(JSON.stringify(result.ops));
+    });
+
+    db.collection('Users').insertOne({
+       name:'zhai',
+        age:24,
+        location:'Hoboken'
+    },(err,result)=>{
+       if(err){
+           return console.log('unable to add user');
+       }
+
+       console.log(result.ops[0]._id);
+    });
+    db.close();
+
+
+});
