db62eeca3577ec713eaf566440d86d4ae820bfe4
y.zhai@partner.samsung.com <y.zhai@partner.samsung.com>
2017-09-25 13:40:51 -0400

    TET

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,10 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+Todo.remove({}).then((result)=>{
+
+});
+
+//findOneAndRemove
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -42,6 +44,43 @@ app.get('/todos/:id',(req,res)=>{
     res.send({todos});
     },(e)=>{
         res.status(400).send(e);
+    });
+});
+
+app.delete('/todos/:id',(req,res)=>{
+   //validate id
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e)=>{
+        return res.status(404).send();
+    });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+
+    if(_.isBoolean(body.completed) &&body.completed){
+        body.completedAt= new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+        res.send({todo});
     });
 });
 
